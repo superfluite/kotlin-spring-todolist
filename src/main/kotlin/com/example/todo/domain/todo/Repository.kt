f4bd5bf1a4d13e6844c5afa9d9ec6367b6eeb39c
Ian <slime@spoqa.com>
2021-11-24 17:10:52 +0900

    review: 불필요한 코드 제거

--- src/main/kotlin/com/example/todo/domain/todo/Repository.kt
+++ src/main/kotlin/com/example/todo/domain/todo/Repository.kt
@@ -3,10 +3,8 @@ package com.example.todo.domain.todo
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
 
-@Repository
 interface TodoRepository : JpaRepository<Todo, Long> {
     fun findAllByIsCompletedOrderByCreatedAtDesc(pageable: Pageable, isCompleted: Boolean): Page<Todo>
 }

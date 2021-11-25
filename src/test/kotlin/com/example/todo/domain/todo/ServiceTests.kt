e8fbb13084f9e3be73b11bf882542a1684cb4316
Ian <slime@spoqa.com>
2021-11-25 14:37:52 +0900

    review: entityManager 쓰지 않고 테스트 하도록 수정

--- src/test/kotlin/com/example/todo/domain/todo/ServiceTests.kt
+++ src/test/kotlin/com/example/todo/domain/todo/ServiceTests.kt
@@ -4,26 +4,21 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.domain.Pageable
-import java.time.LocalDateTime
-import javax.persistence.EntityManager
 import javax.transaction.Transactional
 
 @SpringBootTest
 @Transactional
 class ServiceTests @Autowired constructor(
     val todoService: SimpleTodoService,
-    val entityManager: EntityManager,
 ) {
 
     @Test
     fun `findAll`() {
-        val completedTodo = Todo(content = "todo1", isCompleted = true, createdAt = LocalDateTime.of(2021, 11, 1, 0, 0))
-        val completedTodo2 = Todo(content = "todo2", isCompleted = true, createdAt = LocalDateTime.of(2021, 11, 2, 0, 0))
-        val inCompletedTodo = Todo(content = "todo3")
-        entityManager.persist(completedTodo)
-        entityManager.persist(completedTodo2)
-        entityManager.persist(inCompletedTodo)
-        entityManager.flush()
+        val completedTodo = todoService.create("todo1")
+        val completedTodo2 = todoService.create("todo2")
+        val inCompletedTodo = todoService.create("todo3")
+        todoService.complete(completedTodo.id)
+        todoService.complete(completedTodo2.id)
 
         val actual = todoService.findAll(true, Pageable.ofSize(1))
         assert(actual.count() == 1)
@@ -36,17 +31,16 @@ class ServiceTests @Autowired constructor(
 
     @Test
     fun `create`() {
-        val actual = todoService.create(content = "todo")
+        val actual = todoService.create("todo")
         assert(actual.content == "todo")
         assert(!actual.isCompleted)
+        val actual2 = todoService.findAll(false, Pageable.ofSize(1))
+        assert(actual2.contains(actual))
     }
 
     @Test
     fun `delete`() {
-        val todo = Todo(content = "todo")
-        entityManager.persist(todo)
-        entityManager.flush()
-
+        val todo = todoService.create("todo")
         todoService.delete(todo.id)
         val actual = todoService.findAll(false, Pageable.ofSize(10))
         assert(actual.count() == 0)
@@ -54,9 +48,7 @@ class ServiceTests @Autowired constructor(
 
     @Test
     fun `complete`() {
-        val todo = Todo(content = "todo")
-        entityManager.persist(todo)
-        entityManager.flush()
+        val todo = todoService.create("todo")
 
         assert(!todo.isCompleted)
         todoService.complete(todo.id)

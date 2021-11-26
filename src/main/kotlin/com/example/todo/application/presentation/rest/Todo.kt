eded2e80d1dfe43c87efc56685557bf6c36286ec
Ian <slime@spoqa.com>
2021-11-26 17:28:47 +0900

    review: TodoDTO 추가, 조회 기능에서 TodoDTO 반환하도록 수정

--- src/main/kotlin/com/example/todo/application/presentation/rest/Todo.kt
+++ src/main/kotlin/com/example/todo/application/presentation/rest/Todo.kt
@@ -1,7 +1,11 @@
 package com.example.todo.application.presentation.rest
 
 import com.example.todo.domain.todo.SimpleTodoService
+import com.example.todo.domain.todo.Todo
+import com.example.todo.domain.todo.TodoDTO
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 
@@ -9,7 +13,17 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/todo")
 class TodoController(private val service: SimpleTodoService) {
     @GetMapping("/")
-    fun findAll(@RequestParam(value="isCompleted") isCompleted: Boolean, pageable: Pageable) = service.findAll(isCompleted, pageable)
+    fun findAll(@RequestParam(value="isCompleted") isCompleted: Boolean, pageable: Pageable) : Page<TodoDTO> {
+        val todoList : Page<Todo> = service.findAll(isCompleted, pageable)
+        return todoList.map {
+            todo -> TodoDTO(
+                id = todo.id,
+                createdAt = todo.createdAt,
+                content = todo.content,
+                isCompleted = todo.isCompleted,
+            )
+        }
+    }
 
     @PostMapping("/create/")
     fun create(@RequestBody content: String) = service.create(content)
